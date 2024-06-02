5f291addd33fb34454d2c2e562f553c58b02522f
syedSalman23 <112269253+syedSalman23@users.noreply.github.com>
2024-06-02 09:49:34 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -14,5 +14,5 @@ docker build -t react-ci/cd .
 docker run -d -it --name react -p 80:80 react-ci/cd
 
 #pushing the image to dockerhub:
-docker tag react-ci/cd ravivarman46/react-app:ci-cd
-docker push ravivarman46/react-app:ci-cd
+docker tag react-ci/cd syedsalmann99/react-app:ci-cd
+docker push syedsalmann99/react-app:ci-cd
